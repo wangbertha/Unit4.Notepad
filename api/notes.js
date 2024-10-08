d2c20d42164a05ef96e53fa0eb10a8925009ad3e
wangbertha <wangbertha@gmail.com>
2024-10-08 11:40:23 -0500

    Go through workshop

--- api/notes.js
+++ api/notes.js
@@ -1,1 +1,29 @@
-// TODO: this file :)
+const express = require("express");
+const router = express.Router();
+module.exports = router;
+
+const notes = require("../data/notes");
+
+router.get("/", (req, res) => {
+  res.json(notes);
+});
+
+router.get("/:id", (req, res, next) => {
+  const { id } = req.params;
+  const note = notes.find((n) => n.id === +id);
+  if (note) {
+    res.json(note);
+  } else {
+    next({ status: 404, message: `Note with id ${id} does not exist.`});
+  }
+});
+
+router.post("/", (req, res, next) => {
+  const { text } = req.body;
+  if (text) {
+    notes.push({ id: notes.length + 1, text });
+    res.status(201).json(notes.at(-1));
+  } else {
+    next({ status: 400, message: `New note must have text.`});
+  }
+});
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+app.use(express.json());
 
 // Simple logging middleware
 app.use((req, res, next) => {
@@ -8,32 +9,17 @@ app.use((req, res, next) => {
   next();
 });
 
-// /notes endpoints
-const notes = require("./data/notes");
+app.use('/notes', require('./api/notes'));
 
-app.get("/notes", (req, res) => {
-  res.json(notes);
-});
+app.use((req, res, next) => {
+  next({ status: 404, message: "Endpoint not found." });
+})
 
-app.get("/notes/:id", (req, res, next) => {
-  const { id } = req.params;
-  const note = notes.find((n) => n.id === +id);
-  if (note) {
-    res.json(note);
-  } else {
-    res.status(404).send(`Note with id ${id} does not exist.`);
-  }
-});
-
-app.post("/notes", (req, res, next) => {
-  const { text } = req.body;
-  if (text) {
-    notes.push({ id: notes.length + 1, text });
-    res.status(201).json(notes.at(-1));
-  } else {
-    res.status(400).send(`New note must have text.`);
-  }
-});
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status ?? 500);
+  res.json(err.message ?? "Sorry, something went wrong!");
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
